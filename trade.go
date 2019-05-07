4a06fb1f7f95c7700295a006c6f3c7b86cb30bb8
David Yoon <daveyoon64@fb.com>
2019-05-06 22:35:18 -0700

    lerning about structs and functions

---
+++ trade.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Trade struct {
+	Symbol string
+	Volume int
+	Price  float64
+	Buy    bool
+}
+
+func NewTrade(symbol string, volume int, price float64, buy bool) (*Trade, error) {
+	if symbol == "" {
+		return nil, fmt.Errorf("symbol can't be empty")
+	}
+	if volume <= 0 {
+		return nil, fmt.Errorf("volume must be >= 0 (was %d)", volume)
+	}
+	if price <= 0.0 {
+		return nil, fmt.Errorf("price must be >= 0 (was %f)", price)
+	}
+}
