b54056da0516fc93ae2e4274670da1494c83cb3a
David Yoon <daveyoon64@fb.com>
2019-05-09 22:31:46 -0700

    practice with interfaces

---
+++ interfaces.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Square struct {
+	Length float64
+}
+
+func (s *Square) Area() float64 {
+	return s.Length * s.Length
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c *Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func sumAreas(shapes []Shape) float64 {
+	total := 0.0
+
+	for _, shape := range shapes {
+		total += shape.Area()
+	}
+
+	return total
+}
+
+// shape is a shape interface
+// interface is a collection of methods in Go
+// must match name, signature, parameters, and return type
+type Shape interface {
+	Area() float64
+}
+
+func main() {
+	s := &Square{20}
+	fmt.Println(s.Area())
+
+	c := &Circle{10}
+	fmt.Println(c.Area())
+
+	// kind of acts like an abstract class
+	shapes := []Shape{s, c}
+	sa := sumAreas(shapes)
+	fmt.Println(sa)
+}
